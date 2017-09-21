66fdaa4ff031eff9ce638f2d1b12e495890c7c3b
aniket umbc <aniket.umbc@gmail.com>
2017-09-21 17:00:26 +0530

    'delete-todosbyid-playground-server

--- server/server.js
+++ server/server.js
@@ -20,8 +20,6 @@ app.post('/todos',(req,res)=>{
 var todo=new Todo({
 
     text:req.body.text
-   // Done:req.body.text,
-    //DoneAt:req.body.number
 });
 
 
@@ -58,6 +56,32 @@ res.status(400).send();
 })
 
 });
+
+// Delelte by id 
+
+app.delete('/todos/:id',(req,res)=>{
+
+var id=req.params.id;
+if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+ }
+Todo.findByIdAndRemove(id).then((todo)=>{
+if(!todo)
+     {
+         return res.status(404).send();
+     }
+     res.send({todo});
+}).catch((e)=>{
+res.status(400).send();
+})
+});
+
+
+
+
+
+
+
 
 app.listen(port,()=>{
     console.log(`Hello console Serverup ${port}` );
