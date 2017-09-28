f776a7b8fd0a602877d8991d002dfc8b8a45d768
aniket umbc <aniket.umbc@gmail.com>
2017-09-28 11:12:19 +0530

    'add-config'

---
+++ server/config/config.js
@@ -0,0 +1,22 @@
+var env=process.env.NODE_ENV||'dev';
+
+if(env ==='dev'|| env === 'test'){
+var config=require('./config.json');
+var envConfig=config[env];
+
+Object.keys(envConfig).forEach((key)=>{
+    process.env[key]=envConfig[key];
+});
+}
+
+
+
+
+
+// if(env==='dev'){
+// process.env.PORT=3000;
+// process.env.MONGODB_URI='mongodb://localhost:27017/TodoApp';
+// }else if (env==='test'){
+//     process.env.PORT=3000;
+//     process.env.MONGODB_URI='mongodb://localhost:27017/TodoAppTest';
+// }
